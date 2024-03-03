2b1fe8d0f9c6e737eba7d85cf7d9fc1c3b16e7fd
Zayyzz <fauziahmd1253@gmail.com>
2024-03-03 00:34:29 -0800

    mengerjakan tugas 3

---
+++ kuliah/pertemuan4/latihan2.php
@@ -0,0 +1,24 @@
+<?php
+// sisi kubus a = 9, sisi kubus b= 4
+// hitung total volume kedua kubus
+// $sisi_a = 9;
+// $sisi_b = 4;
+// $volume_a = $sisi_a * $sisi_a *sisi_a;
+// $volume_b = $sisi_b * $sisi_b *sisi_b;
+// $total = $volume_a + $volume_b;
+// echo "Total Volume Kubus A & B adalah = $total";
+// echo "<br>";
+
+function total_volume_dua_kubus($a, $b) {
+    $volume_a = $a * $a * $a;
+    $volume_b = $b * $b * $b;
+    $total = $volume_a +$volume_b;
+
+    return $total;
+
+}
+
+
+echo "Total Volume Kubus A & B = " . total_volume_dua_kubus(9, 4);
+echo "<br>";
+echo "Total Volume Kubus C & D = " . total_volume_dua_kubus(10, 15);
